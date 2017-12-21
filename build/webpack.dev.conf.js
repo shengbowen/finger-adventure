2227447d9a3eb519923fdef279e378596fbd4ad3
bruce.sheng <shengbowen@meizu.com>
2017-12-21 10:28:33 +0800

    add open browser

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -7,6 +7,7 @@ const baseWebpackConfig = require('./webpack.base.conf')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');
 
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
@@ -49,6 +50,9 @@ const devWebpackConfig = merge(baseWebpackConfig, {
       filename: 'index.html',
       template: 'index.html',
       inject: true
+    }),
+    new OpenBrowserPlugin({
+      url: 'http://localhost:' + config.dev.port
     }),
   ]
 })
