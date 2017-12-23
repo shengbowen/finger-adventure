420df7611cf8c53b7c81c112f04e3bd6a73fd18a
bruce.sheng <shengbowen@meizu.com>
2017-12-23 13:57:07 +0800

    add click event

--- src/Game.js
+++ src/Game.js
@@ -2,6 +2,7 @@ import queue from './preload';
 import Floor from './Floor';
 import Robot from './Robot';
 import Leaves from './Leaves';
+import { moveXOffset, moveYOffset } from './config';
 
 class Game {
   constructor() {
@@ -13,8 +14,12 @@ class Game {
     this.canvas.width = window.innerWidth * 2;
     this.canvas.height = window.innerHeight * 2;
     this.stage = new createjs.Stage(this.canvas);
+    this.background = new createjs.Shape();
+    this.background.graphics.beginFill('#001605').drawRect(0, 0, this.canvas.width, this.canvas.height);
+    this.stage.addChild(this.background);
 
     queue.on('complete', this.handleComplete.bind(this));
+    this.bindEvents();
   }
 
   handleComplete() {
@@ -31,6 +36,20 @@ class Game {
       this.leves.tranlateY(50);
       this.stage.update();
     });
+  }
+
+  bindEvents() {
+    this.background.addEventListener('click', this.handleClick.bind(this)); // 必须有元素才会触发，点击空白区域无效
+    //this.stage.addEventListener('click', this.handleClick); // 必须有元素才会触发，点击空白区域无效
+  }
+
+  handleClick(event) {
+    const posX = event.stageX;
+    if (posX > (this.canvas.width/2)) {
+      this.robot.moveRight();
+    } else {
+      this.robot.moveLeft();
+    }
   }
 }
 
